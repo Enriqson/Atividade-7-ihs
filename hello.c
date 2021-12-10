40b3808bf90f2e5375aa1a30fa35b051a265c9d6
Enriqson <51416257+Enriqson@users.noreply.github.com>
2021-12-10 15:30:17 -0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int param = 42;
+
+static int hello_init(void) {
+    printk(KERN_ALERT "Hello world! :)\n");
+    return 0;
+}
+
+static void hello_exit(void) {
+    printk(KERN_ALERT "Goodbye, cruel world! :(\n Param value:%d\n", param);
+}
+
+module_param(param, int, 0);
+module_init(hello_init);
+module_exit(hello_exit);
